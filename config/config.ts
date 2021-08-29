627e57c418ccfd10f17204d7b7be41cc33504579
forsigner <forsigner@gmail.com>
2021-08-29 12:55:00 +0800

    feat: upate config

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,10 @@ import { join } from 'path'
 import { Options } from 'koa-bodyparser'
 import { Auth } from '@shared/interface/auth'
 
+const env = process.env.NODE_ENV || 'development'
+
+const isProd = env === 'production'
+
 class Config {
   port = process.env.PORT || 5001
 
@@ -36,10 +40,15 @@ class Config {
     // 对外(client)暴露的接口，默认 admin 全部可用
     clientWhiteList: [],
 
-    githubOauth: {
-      clientID: '4909444917febd04be0b',
-      clientSecret: 'a9c478e3e13e61e9fcac79efb817f097b922fe91',
-    },
+    githubOauth: isProd
+      ? {
+          clientID: 'a6471688c9cf9b60c68d',
+          clientSecret: '9b319cadda3e5b76400dda951f65e8120f2f7689',
+        }
+      : {
+          clientID: '340a89a4aae1c7d9181f',
+          clientSecret: '3c91b69e4d0af9e4488b8b282c2e27b42993a4eb',
+        },
   }
   debug = true
 }
